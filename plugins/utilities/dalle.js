cf77f0d6fc7e54703fe84b7f14142ef13ff0023b
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-05-16 06:11:40 -0700

    Update dalle.js

--- plugins/utilities/dalle.js
+++ plugins/utilities/dalle.js
@@ -12,8 +12,9 @@ exports.run = {
      if (!text) return client.reply(m.chat, Func.example(isPrefix, command, `girl with red eyes`), m)
          client.sendReact(m.chat, '🕒', m.key)
         let json = await Func.fetchJson(`https://api.itsrose.site/image/diffusion?prompt=${text}&negative_prompt=(worst%20quality%2C%20low%20quality%2C%20extra%20hand)%2C%20monochrome&seed=-1&ratio=9%3A16&style=Realistic&sampler=Euler%20a&cfg=4.0&steps=50&controlNet=openpose&image_num=1&apikey=e3f7c48414c36ab74bbc192f`)
+        const aii = await Func.fetchBuffer(json.result.images)
        let aaaa = `${text}`
-       client.sendFile(m.chat, json.result.images, '', aaaa, m)
+       client.sendFile(m.chat, aii, '', aaaa, m)
      } catch (e) {
          client.reply(m.chat, Func.jsonFormat(e), m)
       }
