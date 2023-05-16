766eb3fd667ab5e7a2eb567226c369690e9cc2a4
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-05-16 06:20:45 -0700

    Update animedif.js

--- plugins/utilities/animedif.js
+++ plugins/utilities/animedif.js
@@ -13,7 +13,7 @@ exports.run = {
          client.sendReact(m.chat, '🕒', m.key)
        let json = await Func.fetchJson(`https://api.itsrose.site/image/diffusion?prompt=${text}&negative_prompt=(worst%20quality%2C%20low%20quality%2C%20extra%20hand)%2C%20monochrome&seed=-1&ratio=9%3A16&style=ACG&sampler=Euler%20a&cfg=4.0&steps=50&controlNet=openpose&image_num=1&apikey=e3f7c48414c36ab74bbc192f`)
        let aaaa = `${text}`
-       client.sendFile(m.chat, json.result.images, '', aaaa, m)
+       client.sendFile(m.chat, json.result.images, 'aaaa.png', aaaa, m)
      } catch (e) {
          client.reply(m.chat, Func.jsonFormat(e), m)
       }
