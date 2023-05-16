51f47da2b85b2140f6ebc6225243cbb7f7dd4169
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-05-16 06:35:25 -0700

    Update animedif.js

--- plugins/utilities/animedif.js
+++ plugins/utilities/animedif.js
@@ -12,7 +12,12 @@ exports.run = {
      if (!text) return client.reply(m.chat, Func.example(isPrefix, command, `girl with red eyes`), m)
          client.sendReact(m.chat, '🕒', m.key)
        let json = await Func.fetchJson(`https://api.itsrose.site/image/diffusion?prompt=${text}&negative_prompt=(worst%20quality%2C%20low%20quality%2C%20extra%20hand)%2C%20monochrome&seed=-1&ratio=9%3A16&style=ACG&sampler=Euler%20a&cfg=4.0&steps=50&controlNet=openpose&image_num=1&apikey=e3f7c48414c36ab74bbc192f`)
-       client.sendFile(m.chat, json.result.images, '', '', m)
+       let caption = `${text}`
+        client.sendMessageModify(m.chat, caption, m, {
+            ads: false,
+            largeThumb: false,
+            thumbnail: await Func.fetchBuffer(json.result.images)
+         })
      } catch (e) {
          client.reply(m.chat, Func.jsonFormat(e), m)
       }
