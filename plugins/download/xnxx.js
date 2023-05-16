36622ebfb6764be0e12825eb3a1ea7e0e38c628e
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-05-16 09:11:12 -0700

    Update xnxx.js

--- plugins/download/xnxx.js
+++ plugins/download/xnxx.js
@@ -16,7 +16,7 @@ exports.run = {
             client.sendReact(m.chat, '🕒', m.key)
             let json = await Api.adus(text)
             if (!json.status) return client.reply(m.chat, global.status.fail, m)
-	  let textt = "*XNXX Search*\n\n Result From search  " + text + "\n\nTo download type ${isPrefix}getxnxx your link\n\n───────────────────\n";
+	  let textt = "*XNXX Search*\n\n Result From search  " + text + "\n\nTo download type " + isPrefix + " getxnxx your link\n\n───────────────────\n";
             json.result.map(async (v, i) => {
 		 textt += `❤️Title : ${v.title}\n⌛️Duration : ${
           v.duration
@@ -32,16 +32,10 @@ exports.run = {
              if (!args[0].match(/(?:https?:\/\/(www\.)?(xnxx)\.(com)\S+)?$/)) return client.reply(m.chat, global.status.invalid, m)
             client.sendReact(m.chat, '🕒', m.key)
             let json = await Func.fetchJson(`https://api-fgmods.ddns.net/api/dowloader/xnxxdl?url=${args[0]}&apikey=fg-dylux`)  
-            let teks = `乂  *N S F W*\n\n`
-            teks += '	◦  *Name* : ' + json.result.title + '\n'
-            teks += '	◦  *Duratiom* : ' + json.result.duration + '\n'
-            teks += '	◦  *Quality* : ' + json.result.quality + '\n'
-            teks += global.footer
-            client.sendFile(m.chat, json.result.image, '', teks, m).then(() => {
-               client.sendFile(m.chat, json.result.files.high, '', json.result.title, m)
-           })
-         }
-      } catch (e) {
+            
+          
+       client.sendFile(m.chat, json.result.files.high, '', '', m)
+           } catch (e) {
          console.log(e)
          return client.reply(m.chat, global.status.error, m)
       }
