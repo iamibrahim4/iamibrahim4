d6a8490fa8c39849a2d15e11e6ac6b8c8d5ec3d9
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-05-17 02:36:13 -0700

    Update aidif.js

--- plugins/utilities/aidif.js
+++ plugins/utilities/aidif.js
@@ -11,8 +11,9 @@ exports.run = {
     try {
         if (!text) return client.reply(m.chat, Func.example(isPrefix, command, `girl with red eyes`), m)
         client.sendReact(m.chat, '🕒', m.key)
-        let json = await Func.fetchJson('https://api.itsrose.site/image/diffusion/txt2img?prompt=${text}&negative_prompt=%20bad%20anatomy%2C%20lowres%2C%20extra%20hands%2C%20extra%20legs%2C%20extra%20finger&width=512&height=512&steps=40&model_id=dream_shaper&sampler=UniPC&cfg=7.5&enhance_prompt=yes&multi_lingual=no&image_num=1&safety_checker=no&panorama=no&hiresFix=no&apikey=e3f7c48414c36ab74bbc192f')
-        client.sendFile(m.chat, json.result.images[0], 'img.png', json.result.generation_time, m)
+        let json = await Func.fetchJson(`https://api.itsrose.site/image/diffusion/txt2img?prompt=${text}&negative_prompt=%20bad%20anatomy%2C%20lowres%2C%20extra%20hands%2C%20extra%20legs%2C%20extra%20finger&width=512&height=512&steps=40&model_id=dream_shaper&sampler=UniPC&cfg=7.5&enhance_prompt=yes&multi_lingual=no&image_num=1&safety_checker=no&panorama=no&hiresFix=no&apikey=e3f7c48414c36ab74bbc192f`)
+        const aii = await Func.fetchBuffer(json.result.images[0])
+        client.sendFile(m.chat, aii, '', json.result.generation_time, m)
      } catch (e) {
          client.reply(m.chat, Func.jsonFormat(e), m)
       }
