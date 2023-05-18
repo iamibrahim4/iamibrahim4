03f2ab3bb00b49b5b3bfd685139677938b9e65e0
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-05-18 03:03:01 -0700

    Update modapk.js

--- plugins/download/modapk.js
+++ plugins/download/modapk.js
@@ -16,7 +16,7 @@ exports.run = {
             client.sendReact(m.chat, '🕒', m.key)
            let json = await Func.fetchJson(`https://api.neoxr.my.id/api/apkmod?q=${text}&apikey=ibrahim9`)
             if (!json.status) return client.reply(m.chat, global.status.fail, m)
-          let textt = "*Mod of app*  " + text + "\n\nTwo dwonload type " + isPrefix + " getmodapk ${text} number of app below \n\n───────────────────\n";
+          let textt = "*Mod of app*  " + text + "\n\nTwo dwonload type " + isPrefix + " getmodapk " + text + " number of app below \n\n───────────────────\n";
           json.data.map(async (v, i) =>  {
                 textt += `⚡ No : ${v.no}\n ❤Name : ${v.name}\n♫ Version : ${
           v.version
@@ -27,10 +27,12 @@ exports.run = {
 	m.reply(textt)
          } else if (command == 'getmodapk') {
             if (!text) return client.reply(m.chat, global.status.invalid, m)
-            let [query, no] = text.split`—`
+            const urut = text.split(' ');
+            let query = urut[0]
+            let no = urut[1]
             client.sendReact(m.chat, '🕒', m.key)
             let json = await Func.fetchJson(`https://api.neoxr.my.id/api/apkmod?q=${query}&no=${no}&apikey=ibrahim9`)
-            let teks = `乂  *P L A Y S T O R E*\n\n`
+            let teks = `乂  *M O D A P K*\n\n`
             teks += '	◦  *Name* : ' + json.data.name + '\n'
             teks += '	◦  *Version* : ' + json.data.version + '\n'
             teks += '	◦  *Size* : ' + json.file.size + '\n'
