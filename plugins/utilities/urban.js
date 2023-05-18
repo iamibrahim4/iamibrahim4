a7a1c9e6ce889249c3b2381c84fd8331930d37d6
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-05-18 03:15:06 -0700

    Create urban.js

---
+++ plugins/utilities/urban.js
@@ -0,0 +1,25 @@
+exports.run = {
+    usage: ['Dictionary'],
+    use: 'query',
+    category: 'information',
+    async: async (m, {
+      client,
+      text,
+      args,
+      isPrefix,
+      command
+    }) => {
+       try {
+        if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m)
+        client.sendReact(m.chat, '🕒', m.key)
+        let json = await Api.urban(text)
+        if (!json.status) return client.reply(m.chat, global.status.fail, m)
+        m.reply(`${json.data.content}\n\n${json.data.author}`)
+        } catch (e) {
+     return client.reply(m.chat, Func.texted('bold', `Can you tell me in deatil`), m)
+  }
+},
+error: false,
+limit: true,
+premium: false,
+}
