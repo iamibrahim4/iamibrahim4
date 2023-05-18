1a82bd7e133e7d5a49e0ec69f654a1a752714109
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-05-18 07:49:59 -0700

    Update aidif.js

--- plugins/utilities/aidif.js
+++ plugins/utilities/aidif.js
@@ -12,8 +12,12 @@ exports.run = {
         if (!text) return client.reply(m.chat, Func.example(isPrefix, command, `girl with red eyes`), m)
         client.sendReact(m.chat, '🕒', m.key)
         let json = await Func.fetchJson(`https://api.itsrose.site/image/diffusion/txt2img?prompt=${text}&negative_prompt=%20bad%20anatomy%2C%20lowres%2C%20extra%20hands%2C%20extra%20legs%2C%20extra%20finger&width=512&height=512&steps=40&model_id=dream_shaper&sampler=UniPC&cfg=7.5&enhance_prompt=yes&multi_lingual=no&image_num=1&safety_checker=no&panorama=no&hiresFix=no&apikey=e3f7c48414c36ab74bbc192f`)
-        client.sendFile(m.chat, json.result.images[0], '.jpeg', `${text}`, m)
-     } catch (e) {
+        for (const [index, url] of json.result["images"].entries()) {
+				setTimeout(async () => {
+					client.sendFile(m.chat,(index === json.result["images"].length - 1, '', '', m)
+						}
+		})
+        } catch (e) {
          client.reply(m.chat, Func.jsonFormat(e), m)
       }
   },
