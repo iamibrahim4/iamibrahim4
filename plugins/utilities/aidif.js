6619ae090a7529c2408724f8bdddfd848729182c
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-05-18 08:07:49 -0700

    Update aidif.js

--- plugins/utilities/aidif.js
+++ plugins/utilities/aidif.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 exports.run = {
   usage: ['aidif'],
   use: 'query',
@@ -11,11 +12,17 @@ exports.run = {
     try {
         if (!text) return client.reply(m.chat, Func.example(isPrefix, command, `girl with red eyes`), m)
         client.sendReact(m.chat, '🕒', m.key)
-        let json = await Func.fetchJson(`https://api.itsrose.site/image/diffusion/txt2img?prompt=${text}&negative_prompt=%20bad%20anatomy%2C%20lowres%2C%20extra%20hands%2C%20extra%20legs%2C%20extra%20finger&width=512&height=512&steps=40&model_id=dream_shaper&sampler=UniPC&cfg=7.5&enhance_prompt=yes&multi_lingual=no&image_num=1&safety_checker=no&panorama=no&hiresFix=no&apikey=e3f7c48414c36ab74bbc192f`)
-        for (const [index, url] of json.result["images"].entries()) {
+        const { data } = await axios
+			.request(`https://api.itsrose.site/image/diffusion/txt2img?prompt=${text}&negative_prompt=%20bad%20anatomy%2C%20lowres%2C%20extra%20hands%2C%20extra%20legs%2C%20extra%20finger&width=512&height=512&steps=40&model_id=dream_shaper&sampler=UniPC&cfg=7.5&enhance_prompt=yes&multi_lingual=no&image_num=1&safety_checker=no&panorama=no&hiresFix=no&apikey=e3f7c48414c36ab74bbc192f`).catch((e) => e?.response);
+       const { status, message, result } = data;
+	    if (!status) {
+			m.reply(message);
+		} else {
+	    for (const [index, url] of json.result["images"].entries()) {
 				setTimeout(async () => {
 					client.sendFile(m.chat,(index === json.result["images"].length - 1), 'image.png', '', m)
 						})
+		} 
 		}
         } catch (e) {
          client.reply(m.chat, Func.jsonFormat(e), m)
