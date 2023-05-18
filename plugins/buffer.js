8866084255a1909c429b088c04cb9e22815629de
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-05-18 08:39:03 -0700

    Update buffer.js

--- plugins/buffer.js
+++ plugins/buffer.js
@@ -1,7 +1,7 @@
 exports.run = {
     usage: ['buffer'],
    use: 'query',
-    category: 'downloader',
+    category: 'tools',
     async: async (m, {
        client,
        text,
@@ -10,8 +10,11 @@ exports.run = {
        command
     }) => {
        try {
-        let thumb = await Func.fetchBuffer(`https://api.itsrose.site/out/85196f74696ba0290e3f5f2557fd0d7b109d5ccee559c210d3c660cbe7a0c54afe1d186b58041e5b1e65a38ad769618796d0fef9dce70bc08712357ac5ede33637fc7925a4ccc01144ea24305c3625efc4da20bc9f28fe30a7079f246ee84b88c4e8b1ed61791a5c3ebcf4`)
-             client.sendFile(m.chat, thumb, '', '', m)
+           if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'your image url'), m);
+      client.sendReact(m.chat, '🕒', m.key);
+             if (text.match(/(?:https?:\/\/?$/)) return client.reply(m.chat, global.status.invalid, m)
+        let thumb = await Func.fetchBuffer(`${text}`)
+        client.sendFile(m.chat, thumb, '', `${text}`, m)
               }
         catch (e) {
           console.log(e)
